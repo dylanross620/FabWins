d2885b9cb164eb44a0f678082424d285dca443d5
Dylan Ross <dylanross620@gmail.com>
2025-08-07 17:03:34 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flesh and Blood Win Counter
+
+This is a small python script to calculate your wins and win rates in paper Flesh and Blood, both with and without including Byes.
+
+To install, run `pip install -r requirements.txt`. To run, just run `python main.py` and follow the prompts. It will ask you for your username (GEM email) and password, these values are not stored anywhere and are only used to authenticate with GEM in order to retrieve your match history.
---
+++ main.py
@@ -0,0 +1,87 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+from getpass import getpass
+
+def login(sess):
+    username = input('Username: ')
+    password = getpass()
+
+    sess.get('https://gem.fabtcg.com')
+    sess.post(
+        url='https://gem.fabtcg.com',
+        data={'csrfmiddlewaretoken': sess.cookies.get('csrftoken'), 'username': username, 'password': password},
+        headers={'Referer': 'https://gem.fabtcg.com/'})
+
+    return 'sessionid' in sess.cookies.keys()
+
+def calculate_pages(sess):
+    resp = sess.get('https://gem.fabtcg.com/profile/history/?role=player', headers={'Referer': 'https://gem.fabtcg.com/profile/history/'})
+    matches = re.findall(r'<a href=\"\?role=player&amp;page=[0-9]+\"', resp.text)
+    num_pages = len(matches)
+    if num_pages == 0:
+        num_pages = 1
+    
+    return num_pages
+
+def parse_page(text):
+    soup = BeautifulSoup(text, 'html.parser')
+
+    games = 0
+    wins = 0
+    byes = 0
+
+    for i, table in enumerate(soup.find_all('table')):
+        if 'Record (W-L' not in table.get_text():
+            continue # Skip any tables that aren't matchup tables
+
+        for i, row in enumerate(table.find_all('tr')):
+            if i == 0:
+                continue # Skip the table header
+
+            result = row.find_all('td')[2].string.strip()
+            games += 1
+            if result == 'Win':
+                wins += 1
+            elif 'Bye' in result:
+                wins += 1
+                byes += 1
+
+    return (games, wins, byes)
+
+def calculate_wins(sess, num_pages):
+    num_games = 0
+    num_wins = 0
+    num_byes = 0
+
+    for i in range(1, num_pages+1):
+        print(f"Parsing page {i} of results")
+        resp = sess.get(f"https://gem.fabtcg.com/profile/history/?role=player&page={i}")
+
+        (games, wins, byes) = parse_page(resp.text)
+        num_games += games
+        num_wins += wins
+        num_byes += byes
+
+    print()
+    print(f"Number of games: {num_games}")
+    if num_games > 0:
+        print(f"Number of wins: {num_wins} ({round(num_wins / num_games * 100)}%)")
+        print(f"Number of byes: {num_byes}")
+        if num_games > num_byes:
+            print(f"Games won: {num_wins - num_byes} ({round((num_wins-num_byes) / (num_games-num_byes) * 100)}%)")
+
+def main():
+    with requests.Session() as sess:
+        if not login(sess):
+            print('Failed to login. Please check your credentials')
+            return
+        else:
+            print('Logged in successfully')
+
+        num_pages = calculate_pages(sess)
+
+        calculate_wins(sess, num_pages)
+
+if __name__ == '__main__':
+    main()
